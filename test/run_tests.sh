0ae8bddedda12b374f5f815552e683ab5b3eb8ad
Tobias Jungel <tobi@bisdn.de>
2014-08-04 17:50:38 +0200

    additional script to run tests in a proper order

---
+++ test/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TESTS_TO_RUN="netconf_get \
+ netconf_create_lsi \
+ netconf_attach_port \
+ netconf_create_lsi2 \
+ netconf_attach_port_invalid \
+ netconf_get_config"
+
+
+for TEST in $TESTS_TO_RUN
+do
+	tput bold
+	echo "running $TEST"
+	tput sgr0
+	
+	./$TEST.exp
+	[ "0" == "$?" ] || {
+		echo "test $TEST failed"
+		exit 1
+	}
+done
