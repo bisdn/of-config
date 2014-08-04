9c2697157ec920d092b0bc7e122adb606838c9d6
Tobias Jungel <tobi@bisdn.de>
2014-08-04 13:33:08 +0200

    use either .password or .passphrase file
    latest version of netconf-cli allows the use of ssh-keys. If the key is
    secured by a passphrase add it to the file .passphrase

--- test/netconf.tcl
+++ test/netconf.tcl
@@ -1,11 +1,26 @@
 proc netconf_connect {} {
 	global expect_out spawn_id
 	
-	# read root password from file
-	set f [open ".password"]
-	set password [read $f]
-	close $f
+	upvar password passphrase
 	
+	set password ""
+	set passphrase ""
+	
+	# read root password from file
+	if [ file exists ".password" ] {
+		set f [open ".password"]
+		set password [read $f]
+		close $f 
+	}
+	
+	
+	# read root passphrase from file
+	if [ file exists ".passphrase" ] {
+		set f [open ".passphrase"]
+		set passphrase [read $f]
+		close $f 
+	}
+		
 	# run netopeer-cli
 	spawn /home/tobi/workspace/netopeer/cli/netopeer-cli
 	expect {
@@ -16,16 +31,20 @@ proc netconf_connect {} {
 	# Send the username, and then wait for a password prompt.
 	send "connect --login root localhost\r"
 	expect {
-		"root@localhost password:"	{}
-		timeout						{ exit 1 }
+		"Enter passphrase for the key" {
+			send "$passphrase\n"
+			exp_continue
+		}
+		
+		"root@localhost password:"	{
+			send "$password\n"
+			exp_continue
+		}
+		"ERROR: Authentication failed."	{ exit 1 }
+		"netconf>" {  }
+		timeout							{ exit 1 }
 	}
 	
-	# Send the password, and then wait for a shell prompt.
-	send "$password\n"
-	expect {
-		"netconf>"	{} 
-		timeout		{ exit 1 }
-	}
 }
 
 proc netconf_disconnect {} {
